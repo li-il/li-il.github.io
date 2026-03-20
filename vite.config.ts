5ef5c30dc4019a2bf04b176b571e70b5ebf204ab
L <l.l@l.home>
2026-03-20 00:10:44 -0400

    udpate vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Replace 'yourusername.github.io' with your repo name
 export default defineConfig({
-  base: '/li-il.github.io/',
+  base: './',
   plugins: [react()]
 })
