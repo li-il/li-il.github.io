1cb28d35b3a7f2c6c10f2a1710d045d31182a56c
L <l.l@l.home>
2026-03-19 23:56:01 -0400

    Initial React portfolio setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'yourusername.github.io' with your repo name
 export default defineConfig({
-  plugins: [react()],
+  base: '/li-il.github.io/',
+  plugins: [react()]
 })
