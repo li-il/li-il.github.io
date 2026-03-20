4d751265553dffa5164ab46e82296d0b835ac2b9
L <l.l@l.home>
2026-03-20 00:29:32 -0400

    date

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace 'yourusername.github.io' with your repo name
-export default defineConfig({
-  base: './',
-  plugins: [react()]
-})
